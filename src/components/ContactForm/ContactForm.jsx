f690930609458de6429f4e5a7b6e421be3b1e6ea
volodymyr.savransky@gmail.com <volodymyr.savransky@gmail.com>
2022-08-27 12:16:14 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,66 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
-export class ContactForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = {
-    name: '',
-    phone: '',
+export function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'phone':
+        setPhone(value);
+        break;
+      default:
+        throw new Error();
+    }
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const { name, phone } = this.state;
-
-    this.props.onSubmit(name, phone);
-    this.setState({ name: '', phone: '' });
+    onSubmit(name, phone);
+    reset();
   };
 
-  render() {
-    const { name, phone } = this.state;
+  const reset = () => {
+    setName('');
+    setPhone('');
+  };
 
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label className={s.formLabel}>
-          <span className={s.formTitle}>Name</span>
-          <input
-            className={s.formInput}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.formLabel}>
+        <span className={s.formTitle}>Name</span>
+        <input
+          className={s.formInput}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
 
-        <label className={s.formLabel}>
-          <span className={s.formTitle}>Phone number</span>
-          <input
-            className={s.formInput}
-            type="tel"
-            name="phone"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            value={phone}
-            onChange={this.handleChange}
-          />
-        </label>
+      <label className={s.formLabel}>
+        <span className={s.formTitle}>Phone number</span>
+        <input
+          className={s.formInput}
+          type="tel"
+          name="phone"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          value={phone}
+          onChange={handleChange}
+        />
+      </label>
 
-        <button className={s.formButton} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+      <button className={s.formButton} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
